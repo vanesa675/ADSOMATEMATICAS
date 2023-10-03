7711a3c9f7088b8bfb33b0ae6ff36eea1e7d4c7a
vanesa675 <lgutierrezlozano09@gmail.com>
2023-10-02 20:45:19 -0500

    se actualizan

---
+++ pruebas de escritorio/js/script.js
@@ -0,0 +1,9 @@
+let num = parseFloat(prompt("ingrese el numero"));
+
+if(num > "+"){
+    alert("el numero es positivo");
+} else if (num == 0) {
+    alert("el numero es 0");
+} else {
+    alert("el numero es negativo")
+}
